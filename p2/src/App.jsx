0d7914c33317a08b31b0c176eab34e4b84d52e77
Janet Choi <jchoi118@terpmail.umd.edu>
2025-09-30 17:38:34 -0400

    working

--- p2/src/App.jsx
+++ p2/src/App.jsx
@@ -3,10 +3,13 @@ import './App.css';
 
 const App = () => {
   // flashcards
-  const flashcardsLst = [ // CHANGE
-    { index: 1, question: "What is React?", answer: "A JavaScript library for building user interfaces." },
-    { index: 2, question: "What is a component?", answer: "A reusable piece of UI." },
-    { index: 3, question: "What is useState?", answer: "A Hook to add state to function components." }
+  const flashcardsLst = [ 
+    { question: "An intergalactic terrorist seeks revenge by hitchhiking a ride with a religious fanatic and a smuggler", answer: "Star Wars: A New Hope" },
+    { question: "Children risk life and limb to harness the fury of wild beasts, in the hopes of attaining fame (or at least some shiny pins)", answer: "Pokemon" },
+    { question: "A boy whose only friend is an eccentric neighbor must shun his mother’s affection or he will cease to exist", answer: "Back To The Future" },
+    { question: "A ragtag team of misfits takes a very long walk to destroy a piece of jewelry.", answer: "The Lord of the Rings Trilogy" },
+    { question: "Reptiles young and old work together to shut down a tropical island resort.", answer: "Jurassic Park" }
+
   ]
 
   // useStates
@@ -15,26 +18,19 @@ const App = () => {
 
   // handling the next button for flashcards
   const handleNext = () => {
-    setFlashcard(currFlashcardIndex + 1)
+    if (currFlashcardIndex < flashcardsLst.length - 1) {
+      setFlashcard(currFlashcardIndex + 1)
+    } else {
+      setFlashcard(0)
+    }
     setFlip(false)
+
   }
   const currFlashcard = flashcardsLst[currFlashcardIndex]; // setting the current flashcard set
 
   // checking if the flashcard is flipped or not
   const checkIfFlipped = () => {
-    setFlip(true) // !prev
-  }
-
-  const flashcardState = ({ currCard, isFlipped }) => {
-    return (
-      <p className='content'>
-        {isFlipped ? (
-          currCard.question
-        ) : (
-          currCard.answer
-        )}
-      </p>
-    );
+    setFlip(prev => !prev)
   }
 
   return (
@@ -43,24 +39,36 @@ const App = () => {
       <div className="header">
         <h1>Guess The Movie</h1>
         <p>Level: poor descriptions! Guess what movie it is based on some funky, vague (but still valid!) movie descriptions.</p>
+        <p>You have {flashcardsLst.length} total cards and you guessed {currFlashcardIndex + 1} cards!</p>
       </div>
 
       {/* -------- content -------- */}
       {/* from w3schools */}
-      <div class="flashcard" onClick={checkIfFlipped}>
-        <div class="flashcard-inner">
+      <div className="flashcard" onClick={checkIfFlipped}>
+        <div className="flashcard-inner">
+
           {/* FRONT OF CARD */}
-          <div class="flashcard-front">
-            {/* <p>{currFlashcard.question}</p> */}
-            {flashcardState({ currCard: currFlashcard, isFlipped: showAnswer })}
+          <div className="flashcard-front">
+            {/* https://react.dev/learn/conditional-rendering#conditional-ternary-operator-- */}
+            <p className='content'>
+              {showAnswer ? (
+                <b>{currFlashcard.answer}</b>
+              ) : (
+                <b>{currFlashcard.question}</b>
+              )}
+            </p>
+            <p className='footer'><i>Credits go to SideShow</i></p>
           </div>
 
         </div>
+      </div>
+
+      <div className='next'>
         <button onClick={handleNext}> Next </button>
       </div>
 
       {/* -------- footer -------- */}
-      <div className="footer">
+      <div className="footer-div">
         <p>Trivia and description credits go to: https://www.sideshow.com/blog/can-you-guess-these-movies-from-misleading-plot-descriptions</p>
       </div>
 
